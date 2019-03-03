96aaa138b72a926d1dd09bd740fee1fced465db0
Vijaya Anand <vanand@apttus.com>
2019-03-03 23:28:40 +0530

    Tiny progress

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var webserver = require('gulp-webserver');
 
 gulp.task('sass', function () {
 	return gulp.src(['./scss', '*.scss'].join('/'))
@@ -13,6 +14,18 @@ gulp.task('sass', function () {
 
 gulp.task('watch', function () {
 	gulp.watch('./scss/*.scss', ['sass']);
+});
+
+gulp.task('server', function () {
+	gulp.src('.').pipe(
+		webserver(
+			{
+				livereload: true,
+				directoryListing: true,
+				open: true
+			}
+		)
+	);
 });
 
 gulp.task('default', ['sass', 'watch']);
