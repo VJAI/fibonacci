7fd2c48293f19ce5f3de403f58e9b4d5651bc545
VJAI <vijay.prideparrot@gmail.com>
2021-12-07 12:13:17 +0530

    detail page fixes

--- fibonacci.js
+++ fibonacci.js
@@ -3,37 +3,36 @@
 	var nav = document.querySelector('.cmp__blog-nav');
 	var searchInput = document.querySelector('.cmp__search-form input');
 	var searchIcon = document.querySelector('.cmp__search-form span');
-
+	
 	searchInput.addEventListener('click', function (e) {
 		e.stopPropagation();
 	});
-
+	
 	f(x);
 	x.addListener(f);
-
+	
 	function f(x) {
-
+		
 		if (x.matches) {
 			searchIcon.removeEventListener('click', searchIconClickHandler);
 		} else {
 			searchIcon.addEventListener('click', searchIconClickHandler);
 		}
 	}
-
+	
 	function searchIconClickHandler(e) {
 		nav.classList.add('search-active');
 		document.body.addEventListener('click', documentClickHandler);
 		searchInput.focus();
 		e.stopPropagation();
 	}
-
+	
 	function documentClickHandler(e) {
 		if (e.currentTarget === searchInput || e.currentTarget === searchIcon) {
 			return;
 		}
-
+		
 		nav.classList.remove('search-active');
 		document.body.removeEventListener('click', documentClickHandler);
 	}
 })();
-
